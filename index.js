d242f509e179a1a5df40c400a852f63f447e4f14
Ankit Pati <contact@ankitpati.in>
2020-03-05 07:16:52 +0530

    test for actions

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const { strictEqual } = require('assert');
 
 const app = new Application({ path: '/opt/E-Parse/eparse' });
 
+let longUrl = 'https://reverse.geocoder.ls.hereapi.com/6.2/' +
+              'reversegeocode.json?prox=41.8839%2C-87.6389%2C150&' +
+              'mode=retrieveAddresses&maxresults=1&additionaldata=' +
+              'IncludeShapeLevel%2CpostalCode&gen=9&apiKey=' +
+              'H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs';
+let expectedHostname = 'reverse.geocoder.ls.hereapi.com';
+
 app.start().then(
     ()           => { return app.client.getText('.brand-logo');    }).then(
     (brand_logo) => { strictEqual(brand_logo, 'G-Parse');          }).catch(
+    (error)      => { console.error('Test failed', error.message); }).then(
+    async ()     => { await app.client.setValue('//*[@id="url"]', longUrl); }).then(
+    ()           => { app.client.click('#submit');                 }).then(
+    async ()     => { await app.client.waitUntilTextExists('//*[@id="hostname"]', expectedHostname); }).then(
+    ()           => { return app.client.getText('//*[@id="hostname"]'); }).then(
+    (hostname)   => { strictEqual(hostname, expectedHostname);     }).catch(
     (error)      => { console.error('Test failed', error.message); }).finally(
     ()           => { return app.stop();                           }).finally(
     ()           => { process.exit(0);                             });
